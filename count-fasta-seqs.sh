ea3d0f25211481888bd87336916cef5b7195130d
Nadia L'Bahy <nlbahy@umass.edu>
2018-06-06 15:08:25 -0500

    Adding commentto file

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -99,3 +99,4 @@ echo "$@"
 # Hey y'all! I'm not actually sure what I should be adding but I wanted to see if this works. :-) - Audrey
 
 #I have no idea either, but this is still fun. I wanted to add something too. - Naudia
+#I finally fixed my mistake and am back in the game -other Nadia 
